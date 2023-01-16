04801cb290d44cdf4ad823474a2940776edc0d3d
Sami DEMIR <96701580+159SAMSPAROW@users.noreply.github.com>
2023-01-16 17:43:42 +0100

    chef, video and laurels component added

--- p9/src/containers/Chef.jsx
+++ p9/src/containers/Chef.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { SubHeading } from '../components'
+import { images } from '../constants'
 
 const Chef = () => {
   return (
-    <div>Chef</div>
+    <div className='app__bg app__wrapper section__padding'>
+      <div className='app__wrapper_img app__wrapper_img-reverse'>
+        <img src={images.chef} alt="chef" />
+      </div>
+
+      <div className='app__wrapper_info'>
+        <SubHeading title="Chef's Word"/>
+        <h1 className='headtext__cormorant'>What We believe In</h1>
+
+        <div className='app__chef-content'>
+          <div className='app__chef-content_quote'>
+            <img src={images.quote} alt="quote" />
+            <p className='p__opensans'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto vel laudantium facilis dolores consectetur</p>
+          </div>
+          <p className='p__opensans'>consectetur adipisicing elit. Iusto vel laudantium facilis dolores consectetur, maiores optio facere itaque accusantium labore doloribus nulla corporis praesentium? Tenetur itaque repellat quasi voluptates fugiat!</p>
+        </div>
+
+        <div className='app__chef-sign'>
+          <p>Kevin luo</p>
+          <p className='p__opensans'>Chef & Founder</p>
+          <img src={images.sign} alt="sign" />
+        </div>
+
+      </div>
+    </div>
   )
 }
 
--- p9/src/containers/Intro.jsx
+++ p9/src/containers/Intro.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
+import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs'
+import { meal } from '../constants'
 
 const Intro = () => {
+  const [playVideo, setPlayVideo] = useState(false)
+  const videoRef = useRef()
+
+  const handleVideo = () => {
+    setPlayVideo((prevPlayVideo) => !prevPlayVideo)
+
+    if (playVideo) {
+      videoRef.current.pause()
+    } else {
+      videoRef.current.play()
+    }
+  }
+
   return (
-    <div>Intro</div>
+    <div className="app__video">
+      <video
+        src={meal}
+        ref={videoRef}
+        type="video/mp4"
+        loop
+        controls={false}
+        muted
+      />
+      <div className="app__video-overlay flex__center">
+        <div
+          className="app__video-overlay_circle flex__center"
+          onClick={handleVideo}
+        >
+          {playVideo ? <BsPauseFill color="#fff"fontSize={30}/> 
+          :
+          <BsFillPlayFill color="#fff"fontSize={30}/>}
+        </div>
+      </div>
+    </div>
   )
 }
 
